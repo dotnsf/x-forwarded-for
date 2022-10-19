b4fb45986dc1b1157eee41c12fb9e4e84bd3559a
K.Kimura <dotnsf@gmail.com>
2022-10-19 18:18:21 +0900

    TRUST_PROXY controlable

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 var express = require( 'express' ),
     app = express();
 
-app.set( 'trust proxy', true );
+if( 'TRUST_PROXY' in process.env && process.env.TRUST_PROXY ){
+  app.set( 'trust proxy', true );
+}
 
 app.get( '/', function( req, res ){
   res.contentType( 'application/json; charset=utf-8' );
@@ -10,7 +12,12 @@ app.get( '/', function( req, res ){
   var json = { status: true, ip: req.ip, remode_address: req.connection.remoteAddress };
   //console.log( req.headers );
   if( req.headers && req.headers['x-forwarded-for'] ){
-    json['x-forwarded-for'] = req.headers['x-forwarded-for'];
+    var tmp = req.headers['x-forwardef-for'].split( ',' );
+    var x = [];
+    for( var i = 0; i < tmp.length; i ++ ){
+      x.push( tmp[i].trim() );
+    }
+    json['x-forwarded-for'] = x;
   }
 
   res.write( JSON.stringify( json, null, 2 ) );
